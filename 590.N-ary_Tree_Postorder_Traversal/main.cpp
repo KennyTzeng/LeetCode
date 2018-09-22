7249b70b0667d66708a99c44b6c8b6608315f238
KennyTseng <kenny29282000@gmail.com>
2018-09-22 15:44:52 +0800

    590: add a solution in C++

---
+++ 590.N-ary_Tree_Postorder_Traversal/main.cpp
@@ -0,0 +1,39 @@
+// #include <vector>
+// using namespace std;
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> result;
+
+    vector<int> postorder(Node* root) {
+        
+        visit(root);
+        return result;
+    }
+
+private:
+    void visit(Node* node) {
+        if(node) {
+            for(Node* child : (node->children)) {
+                visit(child);
+            }
+            result.push_back(node->val);
+        }
+    }
+
+};
